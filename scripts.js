909ed95a6a0f71143f27e3794cc8847a343b8ff1
victoria-st <112092459+victoria-st@users.noreply.github.com>
2022-08-25 16:39:05 +0300

    First commit

---
+++ scripts.js
@@ -0,0 +1,10 @@
+﻿function getId(btn)
+{
+    if ((btn.id) === "2000") {
+        document.getElementById("answer").innerHTML = "American Beauty";
+    } else if ((btn.id) === "2010") {
+        document.getElementById("answer").innerHTML = "The Hurt Locker";
+    } else {
+        document.getElementById("answer").innerHTML = "Parasite";
+    }
+}
